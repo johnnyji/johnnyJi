cdcc24e0df76089bf1a066205c87eef33b9bc5d2
Johnny Ji <johnny@johnnyji.com>
2016-01-17 01:46:57 -0800

    clean crap up

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-$(function onDocumentReady() {
-  'use strict';
-  var replaceLinksWithIcons = function replaceLinksWithIcons(icon, link) {
-    $('.' + icon).replaceWith('<a href=\'' + link + '\' target=\'_blank\' class=\'icon-size\'><i class=\'fa fa-' + icon + '\'></i></a>').addClass('icon-size');
-  };
-  replaceLinksWithIcons('instagram', 'https://www.instagram.com/johnnyisji');
-  replaceLinksWithIcons('linkedin', 'https://www.linkedin.com/pub/johnny-ji/92/1b8/981');
-  replaceLinksWithIcons('twitter', 'https://www.twitter.com/johnnyisji');
-  replaceLinksWithIcons('github', 'https://www.github.com/johnnyji');
+$(function() {
+  // var replaceLinksWithIcons = function replaceLinksWithIcons(icon, link) {
+  //   $('.' + icon).replaceWith('<a href=\'' + link + '\' target=\'_blank\' class=\'icon-size\'><i class=\'fa fa-' + icon + '\'></i></a>').addClass('icon-size');
+  // };
+  // replaceLinksWithIcons('instagram', 'https://www.instagram.com/johnnyisji');
+  // replaceLinksWithIcons('linkedin', 'https://www.linkedin.com/pub/johnny-ji/92/1b8/981');
+  // replaceLinksWithIcons('twitter', 'https://www.twitter.com/johnnyisji');
+  // replaceLinksWithIcons('github', 'https://www.github.com/johnnyji');
 });
