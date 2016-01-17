7d6061e7108a8fb5757d2af4590ba76c2132a4b3
Johnny Ji <johnny@johnnyji.com>
2016-01-17 01:52:10 -0800

    removed jquery

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-$(function() {
-  // var replaceLinksWithIcons = function replaceLinksWithIcons(icon, link) {
-  //   $('.' + icon).replaceWith('<a href=\'' + link + '\' target=\'_blank\' class=\'icon-size\'><i class=\'fa fa-' + icon + '\'></i></a>').addClass('icon-size');
-  // };
-  // replaceLinksWithIcons('instagram', 'https://www.instagram.com/johnnyisji');
-  // replaceLinksWithIcons('linkedin', 'https://www.linkedin.com/pub/johnny-ji/92/1b8/981');
-  // replaceLinksWithIcons('twitter', 'https://www.twitter.com/johnnyisji');
-  // replaceLinksWithIcons('github', 'https://www.github.com/johnnyji');
-});
+(function() {
+  console.log('I coded up this site a long time ago, in a galaxy far far away. Please don\'t take this code even remotely seriously!');
+  console.log('To see a reflection of my more recent work, feel free to contact me or go to https://github.com/johnnyji');
+  console.log('\n');
+  console.log('\n');
+  console.log('May the force be with you.');
+})();
